8444c15b7ceda98182cd6514dddedfdca7e4c70d
TAX2310 <taylor.mreade@gmail.com>
2024-09-19 22:09:27 +0100

    debug

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@
 while :
 do
    echo "Running"
-   TIME = date +%H%M
-   STATE = docker ps -f status
+   TIME =date +%H%M
+   STATE =docker ps -f status
    if [ $TIME > 2200 && $TIME < 0800 ]
    then
       if [ "$( docker container inspect -f '{{.State.Running}}' $container_name )" != "true" ]
